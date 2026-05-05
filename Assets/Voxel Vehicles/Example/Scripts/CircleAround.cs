efa59e17a5c6c15fb6bc24f90168d2eda9d3eda4
yeesh132 <yashs13245@gmail.com>
2026-05-05 16:25:49 +1000

    basic car movement

---
+++ Assets/Voxel Vehicles/Example/Scripts/CircleAround.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircleAround : MonoBehaviour 
+{
+	public float radius;
+	public float timeMultiplier = 0.25f;
+	void Update () 
+	{
+		transform.position = new Vector3(Mathf.Sin(Time.time * timeMultiplier) * radius, 60f + (Mathf.Cos((Time.time / 2f) * timeMultiplier) * 2f), Mathf.Cos(Time.time * timeMultiplier) * radius);
+		transform.LookAt(Vector3.zero);
+	}
+}
---
+++ Assets/Voxel Vehicles/Example/Scripts/SetRandomColor.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetRandomColor : MonoBehaviour 
+{
+	public Color[] colors;
+
+	private Color nextColor = Color.black;
+	private MeshRenderer meshRenderer;
+	private float lastTime;
+
+	void OnEnable()
+	{
+		if (meshRenderer == null)
+			meshRenderer = GetComponent<MeshRenderer>();
+		
+		meshRenderer.material.color = colors[Random.Range(0, colors.Length)];
+		nextColor = colors[Random.Range(0, colors.Length)];
+
+		lastTime = Time.time;
+
+	}
+
+	// Change color over time;
+	void Update()
+	{
+		if (Time.time - lastTime >= 2f)
+		{
+			nextColor = colors[Random.Range(0, colors.Length)];
+
+			while (nextColor == meshRenderer.material.color)
+			{
+				nextColor = colors[Random.Range(0, colors.Length)];
+			}
+				
+			lastTime = Time.time;
+		}
+
+		if (meshRenderer != null)
+			meshRenderer.material.color = Color.Lerp(GetComponent<MeshRenderer>().material.color, nextColor, 2f * Time.deltaTime); 
+	}
+}
